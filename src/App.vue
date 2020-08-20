d307a992e82d4fc8365d6223667b0965a757d72f
Oleh <66703682+OlehBabiak@users.noreply.github.com>
2020-08-20 13:24:40 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+   {{msg}}
+   {{2+2}}
+   {{isTrue ? 'Yes it`s true' : 'NOO!'}}
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  data(){
+    return{
+      msg: 'Hello world',
+      isTrue: true,
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
